4649aee7f4b3902c46b17176debee0d4da17ecd9
lenafwu <lena.f.wu@gmail.com>
2022-10-25 20:35:30 -0400

    add player control, jump, coins

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Exposed to Unity
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+
+    // Invisible in Unity
     private bool jumpKeyPressed;
+    private Rigidbody rb;
+    private float horizontalInput;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,17 +28,44 @@ public class Player : MonoBehaviour
         {
             jumpKeyPressed = true;
         }
-        
+
+        // Get horizontal and vertical input to make the player move
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
 
-    // Check for physics update
+    // FixedUpdate is called one every time physics update
     private void FixedUpdate()
     {
+        // put the velocity at the top line, so the player can move while jumping
+        rb.velocity = new Vector3(horizontalInput, rb.velocity.y, verticalInput);
+
+        // preventing Player from jumping in the air
+        // 1. add a layer to Player
+        // 2. add a Layermask field called Player Mask
+        // 3. in the Player Mask, check Everything except for Player (because the player always colliding with itself)
+        // 4. Physics.OverlapSphere returns an array of the colliders
+        // 5. if length == 0, the player isn't colliding with anything, i.e. in the air
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+
+        // add a force up when jump key is pressed
         if (jumpKeyPressed)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            rb.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
             jumpKeyPressed = false;
+        }
+    }
 
+    // picking up the coins
+    // in coin's collider, check 'is Trigger'
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7) // the layer where the coins are
+        {
+            Destroy(other.gameObject);
         }
     }
 }
